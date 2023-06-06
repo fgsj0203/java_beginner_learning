2e7e59de489cb8e94bc850da22e5f71904b2b3ee
Francisco Gomes <fgsj.dev@outlook.com>
2023-06-06 09:55:12 -0300

    Additing and using function charAt(index String) for read and printing character

--- logic_programming_java/src/InputData.java
+++ logic_programming_java/src/InputData.java
@@ -7,7 +7,6 @@ public class InputData {
         //Example with function locale for use point case decimal
         Locale.setDefault(Locale.US);
 
-
         //Importing library of Scanner for received data input user
         Scanner sc = new Scanner(System.in); //name of variable Scanner is "sc"
 
@@ -29,11 +28,20 @@ public class InputData {
          * If use virgule in decimal house, not modified, if not, use point additing Locale.setDefault(Locale.US)
          * Before declaration Scanner.
          */
-        double x;
-        x = sc.nextDouble();
 
-        //Printing all content input user string in variable "x"
-        System.out.println("Your typed: " + x);
+        /**
+         *double x;
+         *x = sc.nextDouble();
+         *Printing all content input user string in variable "x"
+         *System.out.println("Your typed: " + x);
+         */
+
+        /**
+         * Using function "charAt()" for capture character of string in parameter
+         * char x;
+         * x = sc.next().charAt(0); Obs.: in function charAt(0) a parameter this is index capture in string
+         * System.out.println(x);
+         */
 
         //Finishing resource Scanner
         sc.close();
