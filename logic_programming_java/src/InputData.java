61ddf2bba7a39a3a246bf64c8bbf4c134abd2c5f
Francisco Gomes <fgsj.dev@outlook.com>
2023-06-06 09:37:39 -0300

    file modified additing type integer

--- logic_programming_java/src/InputData.java
+++ logic_programming_java/src/InputData.java
@@ -7,8 +7,15 @@ public class InputData {
         Scanner sc = new Scanner(System.in); //name of variable Scanner is "sc"
 
         //Creating variable
-        String x;
-        x = sc.nextLine(); //Function "nextLine" of received all string input user
+        /**
+         * Using variable of type String
+         * String x;
+         * x = sc.nextLine(); //Function "nextLine" of received all string input user
+         */
+
+        //Using now variable of type "int"
+        int x;
+        x = sc.nextInt();
 
         //Printing all content input user string in variable "x"
         System.out.println("Your typed: " + x);
