f0bb6652778ba5b55f18e94cf2bd14e4e992cf46
Francisco Gomes <fgsj.dev@outlook.com>
2023-06-05 23:43:23 -0300

    code java about input data user, class (Scanner)

---
+++ logic_programming_java/src/InputData.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class InputData {
+    public static void main(String[] args) {
+
+        //Importing library of Scanner for received data input user
+        Scanner sc = new Scanner(System.in); //name of variable Scanner is "sc"
+
+        //Creating variable
+        String x;
+        x = sc.nextLine(); //Function "nextLine" of received all string input user
+
+        //Printing all content input user string in variable "x"
+        System.out.println("Your typed: " + x);
+
+        //Finishing resource Scanner
+        sc.close();
+    }
+}
