1f7cad36ab2a12fdb6d31686d951364d97add870
Francisco Gomes <fgsj.dev@outlook.com>
2023-06-06 09:48:04 -0300

    Additing function Locale for use point in decimal number

--- logic_programming_java/src/InputData.java
+++ logic_programming_java/src/InputData.java
@@ -1,21 +1,36 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class InputData {
     public static void main(String[] args) {
+
+        //Example with function locale for use point case decimal
+        Locale.setDefault(Locale.US);
+
 
         //Importing library of Scanner for received data input user
         Scanner sc = new Scanner(System.in); //name of variable Scanner is "sc"
 
         //Creating variable
         /**
-         * Using variable of type String
-         * String x;
+         *Using variable of type String
+         *string x;
          * x = sc.nextLine(); //Function "nextLine" of received all string input user
          */
 
-        //Using now variable of type "int"
-        int x;
-        x = sc.nextInt();
+        /**
+         *Using now variable of type "int"
+         *int x;
+         *x = sc.nextInt();
+         */
+
+        /**
+         * Using now variable of type "point flutuant".
+         * If use virgule in decimal house, not modified, if not, use point additing Locale.setDefault(Locale.US)
+         * Before declaration Scanner.
+         */
+        double x;
+        x = sc.nextDouble();
 
         //Printing all content input user string in variable "x"
         System.out.println("Your typed: " + x);
