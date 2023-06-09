14703ed6b96e3cfd9eb0695e1828c3f5f6dc2d06
Francisco Gomes <fgsj.dev@outlook.com>
2023-06-08 22:29:37 -0300

    Calculating tax under income

---
+++ beginner_learning_java_beecrowd/src/Structure_Condition/tax_income.java
@@ -0,0 +1,56 @@
+package Structure_Condition;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class tax_income {
+    public static void main(String[] args) {
+
+        /**
+         * Importing library "Locale" for decimal number
+         */
+        Locale.setDefault(Locale.US);
+
+        /**
+         * Importing class Scanner for input data user
+         */
+        Scanner sc = new Scanner(System.in);
+
+        /**
+         * section Variables
+         */
+        double income;
+        double tax_income = 0;
+
+        /**
+         * Input data user
+         */
+        System.out.println("Enter with income: ");
+        income = sc.nextDouble();
+
+        /**
+         * Structure condition of calculate income tax
+         */
+        if (income > 0 && income <= 2000.00){
+            System.out.println("Tax income Foolish");
+        } else if (income > 2000.01 && income <= 3000.00) {
+            tax_income = (income * 0.08);
+        } else if (income > 3000.01 && income < 4500.00) {
+            tax_income = (income * 0.18) + ();
+        } else if (income > 4500.00) {
+            tax_income = (income * 0.28);
+        } else {
+            System.out.println("Out interval");
+        }
+
+        /**
+         * Printing final value tax income
+         */
+        System.out.println("Your tax is: U$ " + tax_income);
+
+        /**
+         * Ending resource Scanner
+         */
+        sc.close();
+    }
+}
