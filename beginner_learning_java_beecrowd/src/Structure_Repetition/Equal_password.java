69f6f950940b9fac1f1e437b84ed179f24cb08b0
Francisco Gomes <fgsj.dev@outlook.com>
2023-06-13 23:09:12 -0300

    Code java: Comparing values of two strings name and password

---
+++ beginner_learning_java_beecrowd/src/Structure_Repetition/Equal_password.java
@@ -0,0 +1,53 @@
+package Structure_Repetition;
+
+import java.util.Scanner;
+
+public class Equal_password {
+    public static void main(String[] args) {
+
+        /**
+         * Importing class Scanner for input data user
+         */
+        Scanner sc = new Scanner(System.in);
+
+        /**
+         * Section of variables and input data user
+         */
+        System.out.println("Enter with name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter with password: ");
+        String password = sc.nextLine();
+
+        /**
+         * Verify of values is equal and finished program
+         */
+        if (name.equals(password)){
+            System.out.println("name and password is equal");
+        } else {
+            System.out.println("Continue a program...");
+        }
+
+        /**
+         * Logical Structure for printing final value
+         */
+        while (!name.equals(password)){
+            System.out.println("Name not equal a password, try again");
+            System.out.println("Your name: ");
+            name = sc.nextLine();
+
+            System.out.println("your password: ");
+            password = sc.nextLine();
+        }
+
+        /**
+         * Printing value final
+         */
+        System.out.printf("Your name is equal a password, name: %s and password: %s", name,password);
+
+        /**
+         * Ending resource "sc" of Scanner
+         */
+        sc.close();
+
+    }
+}
