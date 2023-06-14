f235554cd02d11a790fa4f164407387f45f8a8dd
Francisco Gomes <fgsj.dev@outlook.com>
2023-06-14 15:05:31 -0300

    Code Java: Printing numbers in interval 0-20

---
+++ beginner_learning_java_beecrowd/src/Structure_Repetition/Interval_0_20.java
@@ -0,0 +1,20 @@
+package Structure_Repetition;
+
+import java.util.Scanner;
+
+public class Interval_0_20 {
+    public static void main(String[] args) {
+        /**
+         * Importing class Scanner for input data user
+         */
+        Scanner sc = new Scanner(System.in);
+
+        /**
+         * Creating structure logical for printing numbers in interval
+         */
+
+        for (int i = 0; i < 21; i++) {
+            System.out.println(i);
+        }
+    }
+}
