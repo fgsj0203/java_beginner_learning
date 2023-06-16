46c4ccb1360a50ffacd698ac791f9751399f3ff3
Francisco Gomes <fgsj.dev@outlook.com>
2023-06-15 23:57:46 -0300

    File code java modified: Additing new command of substring and include comments

--- beginner_learning_java_beecrowd/src/Examples_String.java
+++ beginner_learning_java_beecrowd/src/Examples_String.java
@@ -17,14 +17,23 @@ public class Examples_String {
         String s01 = original.toLowerCase();
 
         //Converting all string for upper
+        //Convertendo toda a string em maiúsculo
         String s02 = original.toUpperCase();
 
         //Removing space empty of String
+        //Removendo os espaços em brancos da string
         String s03 = original.trim();
 
         //Using only one parameter for initial position until final position
         //Usando o comando substring e colocar como parâmetro a partir de onde começar a buscar a String até o final
         String s04 = original.substring(2);
+
+        //Using command substring for capture position initial of string and second argument of limit String
+        /**
+         * Usando comando Substring para capturar criar uma nova string
+         * A partir de dois argumentos, o da posição incial e até aonde ela vai ter que buscar
+         */
+        String s05 = original.substring(2,10);
 
         /**
          * Printing demonstration
@@ -34,6 +43,7 @@ public class Examples_String {
         System.out.println(s02);
         System.out.println(s03);
         System.out.println(s04);
+        System.out.println(s05);
 
         /**
          * Ending resource Scanner
