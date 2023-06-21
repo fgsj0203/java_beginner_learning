12a9ce405bc235240b669cda1cca01f255c5ee97
Francisco Gomes <fgsj.dev@outlook.com>
2023-06-20 22:41:45 -0300

    Code java: First example about Programming oriented object

---
+++ beginner_learning_java_beecrowd/src/Entities/Main.java
@@ -0,0 +1,11 @@
+package Entities;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
---
+++ beginner_learning_java_beecrowd/src/Entities/Triangle.java
@@ -0,0 +1,7 @@
+package Entities;
+
+public class Triangle {
+
+    double aX, bX, cX, aY, bY, cY;
+
+}
